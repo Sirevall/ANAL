83527d9ba3125686c12aa70a9772d338cc6be214
Mikhail Kovtun <sirevall@gmail.com>
2025-02-02 22:18:59 +0300

    update files with comments

--- TEST_main.py
+++ TEST_main.py
@@ -1,20 +1,41 @@
 import psycopg2
 import requests
-import time
-from config import host, user, password, db_name
+import os
+import psycopg2.extras
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_query(query_name):
+    with open("queries.sql", "r", encoding="utf-8") as f:
+        content = f.read().strip().split("\n\n")  # Разделяем запросы по пустым строкам
+        for block in content:
+            lines = block.strip().split("\n")
+            key = lines[0].split("=")[0].strip()  # Берем название запроса (ключ)
+            value = "\n".join(lines[1:]).strip()  # Берем сам SQL-запрос
+            if key == query_name:
+                return value
+    raise ValueError(f"SQL-запрос '{query_name}' не найден в файле queries.sql")
+
+def execute_query(query_name):
+    try:
+        conn, cursor = connect_db()
+        sql_query = get_query(query_name)
+        cursor.execute(sql_query)
+        print(f"Запрос выполнен: {query_name}")
+    except Exception as e:
+        print(f"Ошибка выполнения SQL-запроса '{query_name}': {e}")
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
 
 def create_table():
     try:
+        create_main_table_query = get_query("CREATE_MAIN_TABLE")
         conn, cursor = connect_db()
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS crypto_rates_raw_h1 (
-            asset VARCHAR (100),
-            datetime TIMESTAMP,
-            rate FLOAT (8)
-        );
-        """)
+        cursor.execute(create_main_table_query)
         print("Таблица успешно создана или существует.")
     
     except Exception as e:
@@ -27,25 +48,23 @@ def create_table():
 
 def connect_db():
     conn = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        dbname=db_name
-        )
+        host=os.getenv('HOST'),
+        user=os.getenv('USER'),
+        password=os.getenv('PASSWORD'),
+        dbname=os.getenv('DB_NAME')
+    )
     conn.autocommit = True
     cursor = conn.cursor()
 
     cursor.execute("SET search_path TO sirevall;")
-    return conn,cursor
+    return conn, cursor
             
 def clear_table():
     try:
+        table_for_cleaning = get_query("TRUNCATE_TABLE")
         conn, cursor = connect_db()
-
-        cursor.execute("""
-        TRUNCATE TABLE crypto_rates_raw_h1;
-        """)
-        print("Насрано... Убираем")
+        cursor.execute(table_for_cleaning)
+        print("Все подчищено")
     
     except Exception as e:
         print(f"Ошибка при подключении к базе данных: {e}")
@@ -63,72 +82,60 @@ def get_data_capacity():
     print(f"Найдено {len(assets)} активов")
     return assets
 
-def write_data(start_date, data, days):
+def fetch_crypto_data(start_date, data, days, asset_limit=None):
+    
+    base_url = "https://api.coincap.io/v2/assets/"
+    start_timestamp = int(datetime.strptime(start_date, "%Y-%m-%d").timestamp() * 1000)
+    end_timestamp = int((datetime.strptime(start_date, "%Y-%m-%d") + timedelta(days)).timestamp() * 1000)
+    
+    all_data = []
+    asset_limit = min(asset_limit, len(data)) if asset_limit else len(data)
+    assets_to_process = data[:asset_limit]
+
+    for asset in assets_to_process:
+        asset_id = asset["id"]
+        try:
+            history_url = f"{base_url}{asset_id}/history?interval=h1&start={start_timestamp}&end={end_timestamp}"
+            history_response = requests.get(history_url)
+            history_response.raise_for_status()
+            history_data = history_response.json()["data"]
+
+            for entry in history_data:
+                all_data.append((
+                    asset_id,
+                    datetime.fromtimestamp(entry["time"] / 1000),
+                    float(entry["priceUsd"])
+                ))
+
+        except requests.exceptions.RequestException as e:
+            print(f"Ошибка при запросе к API ({asset_id}): {e}")
+            continue
+    print("Данные удачно получены")
+    return all_data
+
+def insert_crypto_data(all_data):
+    if not all_data:
+        print("Нет данных для записи в БД")
+        return
+
     try:
-        base_url = "https://api.coincap.io/v2/assets/"
-    
-        start_timestamp = int(datetime.strptime(start_date, "%Y-%m-%d").timestamp() * 1000)
-        end_timestamp = int((datetime.strptime(start_date, "%Y-%m-%d") + timedelta(days)).timestamp() * 1000)
-        
         conn, cursor = connect_db()
-        current_asset = 1
-    
-        for asset in data:
-            asset_id = asset["id"]
-            try:                
-                history_url = f"{base_url}{asset_id}/history?interval=h1&start={start_timestamp}&end={end_timestamp}"
-                history_response = requests.get(history_url)
-                history_response.raise_for_status()
-                history_data = history_response.json()["data"]
-                
-                for entry in history_data:
-                    insert_query = '''
-                    INSERT INTO crypto_rates_raw_h1 (asset, datetime, rate)
-                    VALUES (%s, %s, %s)
-                    '''
-                    cursor.execute(insert_query, (
-                        asset_id,
-                        datetime.fromtimestamp(entry["time"] / 1000),
-                        float(entry["priceUsd"])
-                    ))
-                                
-            except Exception as e:
-                print(f"Ошибка при подключении к базе данных: {e}")
-                continue
-            except requests.exceptions.RequestException as e:
-                print(f"Ошибка при запросе к API: {e}")
-            finally:
-                print(f"Добавлены данные:{asset_id}-{current_asset}")
-                current_asset += 1
+        insert_query = get_query("INSERT_DATA")
+        psycopg2.extras.execute_values(cursor, insert_query, all_data)
+        print(f"Добавлено {len(all_data)} записей в базу данных.")
+    except Exception as e:
+        print(f"Ошибка при записи в базу данных: {e}")
     finally:
         if conn:
             cursor.close()
             conn.close()
-            print("Закрыто соединение")
+            print("Закрыто соединение с базой данных.")
 
 def sql_from_dbeaver():
     try:
+        create_agg_table_query = get_query("CREATE_AGG_TABLE")
         conn, cursor = connect_db()
-
-        cursor.execute("""
-        CREATE TABLE sirevall.crypto_rates_agg AS
-        SELECT
-            asset,
-            datetime AS hour,
-            AVG(rate) AS avg_price,
-            MAX(rate) AS max_price,
-            MIN(rate) AS min_price,
-            AVG(rate) OVER (PARTITION BY asset, DATE(datetime)) AS daily_avg_price
-        FROM
-            sirevall.crypto_rates_raw_h1
-        GROUP BY
-            asset,
-            datetime,
-            rate
-        ORDER BY
-            asset,
-            hour;
-        """)
+        cursor.execute(create_agg_table_query)
         print("Таблица успешно создана или существует.")
     
     except Exception as e:
@@ -145,7 +152,9 @@ if __name__ == "__main__":
     clear_table()
     data = get_data_capacity()
     days = 30
+    asset_count = 10
     start_date = "2024-11-01"
-    write_data(start_date, data, days)
+    data_for_insert = fetch_crypto_data(start_date, data, days, asset_count)
+    insert_crypto_data(data_for_insert)
     sql_from_dbeaver()
     print("канэц")
